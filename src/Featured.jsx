c0dc66bf999efb124193fbcd6fe1c30adf23df1c
ironkamosi <alintonchambers@gmail.com>
2021-04-09 19:16:57 +0200

    removed comments

--- src/Featured.jsx
+++ src/Featured.jsx
@@ -1,14 +1,5 @@
-import "./Feature.css"
+import "./Feature.css";
 const Featured = ({ featuredJob }) => {
-  //   return featuredJob ? (
-  //     <Badge pill variant="secondary">
-  //       {" "}
-  //       Secondary
-  //     </Badge>
-  //   ) : (
-  //     <></>
-  //   );
-  //pill variant="secondary"
   if (featuredJob === true) {
     return (
       <div className="feature-pill">
--- src/PillBadge.jsx
+++ src/PillBadge.jsx
@@ -1,22 +1,11 @@
 // import { Badge } from "react-bootstrap";
 import "./PillBadge.css"
 const PillBadge = ({ text,dark }) => {
-  //   return featuredJob ? (
-  //     <Badge pill variant="secondary">
-  //       {" "}
-  //       Secondary
-  //     </Badge>
-  //   ) : (
-  //     <></>
-  //   );
-  // pill variant="secondary"
-  // if (newJob === true) {
+
   return <div className={`new-badge ${dark && "dark"}`}>
     <p className="button-text">{text}</p>
   </div>;
-  // } else {
-  //   return null
-  // }
+ 
 };
 
 export default PillBadge;
